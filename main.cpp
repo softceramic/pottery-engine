a4ee15b88fe29e0ca8663fef6fa0a6c3d291a736
ali <adam179841@gmail.com>
2025-09-14 00:43:55 -0300

    init

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+class Triangle
+{
+public:
+    void run()
+    {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanUp();
+    }
+
+private:
+    GLFWwindow *window;
+    void initWindow() {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        window = glfwCreateWindow(WIDTH,HEIGHT,"Vulkan",nullptr,nullptr);
+    }
+    void mainLoop() {
+        while(!glfwWindowShouldClose(window)){
+            glfwPollEvents();
+        }
+    }
+    void cleanUp() {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+    void initVulkan() {}
+
+};
+    int main()
+    {
+        Triangle app;
+        try
+        {
+            app.run();
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << e.what() << std::endl;
+            return EXIT_FAILURE;
+        }
+        return EXIT_SUCCESS;
+    }
